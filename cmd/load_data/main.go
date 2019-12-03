49147849ee67482e89a1d2367f9eba22bd914691
vietvudanh <vietvudanh@gmail.com>
2019-12-03 21:58:17 +0700

    Fix the error with concurrency. You need context man.

--- cmd/load_data/main.go
+++ cmd/load_data/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"arango"
 	"bufio"
+	"context"
 	"github.com/arangodb/go-driver"
 	"log"
 	"os"
@@ -14,13 +15,13 @@ import (
 func main() {
 	file, err := os.Open("/Users/vietvu/workspace/data/facebook_clean_data/artist_edges.csv")
 	if err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
-	defer func(){
-	    err := file.Close()
-	    if err != nil {
-	        log.Fatalln("cannot close file", file.Name())
-	    }
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Fatalln("cannot close file", file.Name())
+		}
 	}()
 
 	c := arango.GetClient()
@@ -28,7 +29,7 @@ func main() {
 
 	//
 	var collection driver.Collection
-	if ok, _ := db.CollectionExists(nil, "artist") ; ok {
+	if ok, _ := db.CollectionExists(nil, "artist"); ok {
 		collection, _ = db.Collection(nil, "artist")
 	} else {
 		collection, _ = db.CreateCollection(nil, "artist", nil)
@@ -39,7 +40,7 @@ func main() {
 	vertices := make(map[string]bool)
 	edges := make([][]string, 0, 100)
 	for scan.Scan() {
-	  	line := scan.Text()
+		line := scan.Text()
 		row := strings.Split(line, ",")
 		from, to := row[0], row[1]
 		vertices[from] = true
@@ -56,9 +57,9 @@ func main() {
 	}
 
 	// vertices
+	ctxAwait := driver.WithWaitForSync(context.Background())
 	wp := workerpool.New(4)
 	buffer := make([]map[string]string, 0, 10000)
-	log.Println(len(buffer))
 	i := 0
 	for v, _ := range vertices {
 		buffer = append(buffer, map[string]string{
@@ -66,33 +67,30 @@ func main() {
 		})
 
 		if len(buffer) == 10000 {
-			insert := make([]map[string]string, 0, 10000)
-			copy(insert, buffer)
-			log.Println(i, "insert vertices in ::", len(insert))
 			i := i
+			e := buffer
 			wp.Submit(func() {
 				log.Println("w vertices", i)
-				collection.CreateDocuments(nil, insert)
+				collection.CreateDocuments(ctxAwait, e)
 			})
 			buffer = []map[string]string{}
 		}
 		i += 1
 	}
 	log.Println("insert vertices out::", len(buffer))
-	collection.CreateDocuments(nil, buffer)
+	collection.CreateDocuments(ctxAwait, buffer)
 	wp.StopWait()
 
 	// edges
 	var graph driver.Graph
-	if ok, _ := db.GraphExists(nil, "graph_artist") ; ok {
+	if ok, _ := db.GraphExists(nil, "graph_artist"); ok {
 		graph, _ = db.Graph(nil, "graph_artist")
 	} else {
 		graph, _ = db.CreateGraph(nil, "graph_artist", nil)
 	}
 
-
 	var edgeCollection driver.Collection
-	if ok, _ := graph.EdgeCollectionExists(nil, "artist_edge") ; ok {
+	if ok, _ := graph.EdgeCollectionExists(nil, "artist_edge"); ok {
 		edgeCollection, _, _ = graph.EdgeCollection(nil, "artist_edge")
 	} else {
 		edgeCollection, _ = graph.CreateEdgeCollection(nil, "artist_edge", driver.VertexConstraints{
@@ -105,25 +103,24 @@ func main() {
 	wp = workerpool.New(4)
 	bufferEdges := make([]map[string]string, 0, 10000)
 	for i, edge := range edges {
-		fIdNode, tIdNode := "artist/" + edge[0], "artist/" + edge[1]
+		fIdNode, tIdNode := "artist/"+edge[0], "artist/"+edge[1]
 		bufferEdges = append(bufferEdges, map[string]string{
 			"_from": fIdNode,
-			"_to": tIdNode,
+			"_to":   tIdNode,
 		})
 
 		if len(bufferEdges) == 10000 {
-			insert := make([]map[string]string, 0, 10000)
-			copy(insert, bufferEdges)
-			log.Println(i, "insert edges in::", len(insert))
+			i := i
+			e := bufferEdges
 			wp.Submit(func() {
 				log.Println("w edges", i)
-				edgeCollection.CreateDocuments(nil, insert)
+				edgeCollection.CreateDocuments(ctxAwait, e)
 			})
 			bufferEdges = []map[string]string{}
 		}
 	}
 	log.Println("insert edges out::", len(bufferEdges))
-	edgeCollection.CreateDocuments(nil, bufferEdges)
+	edgeCollection.CreateDocuments(ctxAwait, bufferEdges)
 	wp.StopWait()
 
 	log.Println("done")
